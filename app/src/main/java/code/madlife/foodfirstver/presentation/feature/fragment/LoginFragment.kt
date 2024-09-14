a4960ccfaec0b1795552919a151f374ce2f78988
Minh <dinhthanhminhk11.git@gmail.com>
2024-09-14 18:32:25 +0700

    update ui login

--- app/src/main/java/code/madlife/foodfirstver/presentation/feature/fragment/LoginFragment.kt
+++ app/src/main/java/code/madlife/foodfirstver/presentation/feature/fragment/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import code.madlife.foodfirstver.R
 import code.madlife.foodfirstver.databinding.FragmentLoginBinding
 import code.madlife.foodfirstver.databinding.FragmentUserBinding
@@ -15,23 +14,23 @@ import code.madlife.foodfirstver.presentation.core.base.BaseFragment
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
 
     companion object {
-//        const val IS_GO_FROM_DETAIL = "IS_GO_FROM_DETAIL"
-//        const val NEW_ID = "NEW_ID"
-//        fun newInstance(goFromDetail: Boolean? = null, newsId: String? = null): LoginFragment {
-//            val fragment = LoginFragment()
-//            val args = Bundle()
-//            if (goFromDetail != null) {
-//                args.putBoolean(IS_GO_FROM_DETAIL, goFromDetail)
-//            }
-//            args.putString(NEW_ID, newsId)
-//            fragment.arguments = args
-//            return fragment
-//        }
+        const val IS_GO_FROM_DETAIL = "IS_GO_FROM_DETAIL"
+        const val NEW_ID = "NEW_ID"
+        fun newInstance(goFromDetail: Boolean? = null, newsId: String? = null): LoginFragment {
+            val fragment = LoginFragment()
+            val args = Bundle()
+            if (goFromDetail != null) {
+                args.putBoolean(IS_GO_FROM_DETAIL, goFromDetail)
+            }
+            args.putString(NEW_ID, newsId)
+            fragment.arguments = args
+            return fragment
+        }
     }
 
     override fun initView() {
         binding.login.setOnClickListener {
-            findNavController().popBackStack()
+            NavigationManager.getInstance().popBackStack()
         }
     }
 
